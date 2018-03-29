0b72494f4522cec0ab0496c38c1251da4d445032
Marco L. Abreu <marcolabreu@royalmail.top>
2018-03-29 12:46:21 +0200

    Update install sh script to Photoshop CC 2018 location

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo 'Copying script to /Applications/Adobe Photoshop CC 2014/Preset/Scripts/....'
+echo 'Copying script to /Applications/Adobe Photoshop CC 2018/Preset/Scripts/....'
 
-cp Create-iOS-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
-cp Create-iMessage-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
-cp Create-iMessage-stickers.jsx /Applications/Adobe\ Photoshop\ CC\ 2014/Presets/Scripts/
+cp Create-iOS-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2018/Presets/Scripts/
+cp Create-iMessage-icons.jsx /Applications/Adobe\ Photoshop\ CC\ 2018/Presets/Scripts/
+cp Create-iMessage-stickers.jsx /Applications/Adobe\ Photoshop\ CC\ 2018/Presets/Scripts/
 
-echo '🎉   Done! 🍺'
+echo 'Done!'
